13adcf87559d3c5f64836b53a352119201ca5c64
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2025-04-16 11:53:46 +0100

    CHORE: setting up project structure

---
+++ abradio/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-indigo-600 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">🚀 Tailwind v4 is working!</h1>
+    </div>
+  );
+}
