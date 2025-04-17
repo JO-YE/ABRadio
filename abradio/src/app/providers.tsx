b5cc892278dbab7539faf06684d29e185539caa2
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2025-04-17 06:58:12 +0100

    feat: seeting up the app provider

---
+++ abradio/src/app/providers.tsx
@@ -0,0 +1,19 @@
+// src/app/providers.tsx
+'use client';
+
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { store } from '@/redux/store';
+import { ReactNode, useState } from 'react';
+
+export function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </Provider>
+  );
+}
